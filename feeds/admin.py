cb76b787acee5fc67533dc08af1b4a5888095871
Rob Berry <rob@apple.(none)>
2008-09-21 12:29:26 +0100

    setup admin

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,5 +1,9 @@
 from feeds.models import Feed, Post
 from django.contrib import admin
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'feed', 'updated')
+    list_filter = ('feed',)
+
 admin.site.register(Feed)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
