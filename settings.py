7edd95436d9ef58c81536ae885b42c966b3eeaa4
Rob Berry <rob@apple.(none)>
2008-12-28 01:02:25 +0000

    added the context_processor

--- settings.py
+++ settings.py
@@ -62,6 +62,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'feeds.context_processors.feeds',
 )
 
 
