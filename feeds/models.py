dd674a9d9b0f3fdb76b96a5cbfc4c8c81befb639
Rob Berry <rob@apple.(none)>
2008-12-28 01:01:46 +0000

    use signal to process feed on saving

--- feeds/models.py
+++ feeds/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from feeds.tools import FeedProcessor
+
 import feedparser
 from datetime import datetime
 import time
+
+def feed_creation(sender, **kwargs):
+    if isinstance(kwargs['instance'], Feed) and kwargs['created']:
+        fp = FeedProcessor(Post)
+        fp.process_feed(kwargs['instance'])
+models.signals.post_save.connect(feed_creation)
+
 
 class Site(models.Model):
     name = models.CharField(_('name'), max_length=100)
@@ -21,6 +30,10 @@ class Site(models.Model):
     def __unicode__(self):
         return u'%s' % self.name
 
+
+class FeedManager(models.Manager):
+    pass
+
 class Feed(models.Model):
     feed_url = models.URLField(_('feed url'), unique=True)
 
@@ -36,8 +49,11 @@ class Feed(models.Model):
 
     # http://feedparser.org/docs/http-etag.html
     etag = models.CharField(_('etag'), max_length=50, blank=True, null=True)
-    last_modified = models.DateTimeField(_('last modified'), null=True, blank=True)
+    updated = models.DateTimeField(_('last modified'), null=True, blank=True)
     last_checked = models.DateTimeField(_('last checked'), null=True, blank=True)
+
+    # manager
+    objects = FeedManager()
 
     class Meta:
         verbose_name = _('feed')
@@ -45,40 +61,46 @@ class Feed(models.Model):
         ordering = ('name', 'feed_url',)
 
     def __unicode__(self):
-        return u'%s (%s)' % (self.name, self.feed_url)
+        return u'%s' % (self.name,)
 
     def save(self):
-        if not self.id:
+        if not self.id or True:
             item = feedparser.parse(self.feed_url)
             self.title = item.feed.title
             self.link = item.feed.link
             self.etag = item.etag
-            self.tagline = item.tagline
+            self.tagline = item.feed.tagline
             if hasattr(item, 'modified'):
                 modified = list(item.modified[0:8]) + [-1]
                 modified = datetime.fromtimestamp(time.mktime(modified))
             else:
-                modified = datetime.now()
-            self.last_modified = modified
+                modified = None
+            self.updated = modified
             self.last_checked = datetime.now()
-        super(Feed, self).save()
+        super(self.__class__, self).save()
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('feed_detail', str(self.id))
 
 class Post(models.Model):
     feed = models.ForeignKey(Feed, verbose_name=_('feed'), null=False, blank=False)
     title = models.CharField(_('title'), max_length=255)
     link = models.URLField(_('link'), )
     content = models.TextField(_('content'), blank=True)
-    date_modified = models.DateTimeField(_('date modified'), null=True, blank=True)
+    filtered_content = models.TextField(_('content'), blank=True)
     guid = models.CharField(_('guid'), max_length=200, db_index=True)
     author = models.CharField(_('author'), max_length=50, blank=True)
     author_email = models.EmailField(_('author email'), blank=True)
     comments = models.URLField(_('comments'), blank=True)
-    date_created = models.DateField(_('date created'), auto_now_add=True)
+    created = models.DateField(_('date created'), auto_now_add=True)
+    updated = models.DateTimeField(_('date modified'), null=True, blank=True)
+    read = models.BooleanField(_('has this post been read'), default=False)
 
     class Meta:
         verbose_name = _('post')
         verbose_name_plural = _('posts')
-        ordering = ('-date_modified',)
+        ordering = ('-updated',)
         unique_together = (('feed', 'guid'),)
 
     def __unicode__(self):
@@ -86,3 +108,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return self.link
+
+    # def save(self, *args, **kwargs):
+    #     pass
