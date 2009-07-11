ae99ed954a735f017d5dccc7f5726bfe2c490c03
Rob Berry <rob@luvhate.us>
2009-07-11 13:15:01 +0100

    stop using local.py

--- local.py
+++
@@ -1,5 +0,0 @@
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'M*6lock'         # Not used with sqlite3.
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'qb5k_xmwq0dbk1t+pgk4pa0&gbst^!5mnr4$4)bb=)-2t3wsw9'
-
