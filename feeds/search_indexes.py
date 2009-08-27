c5a1acb3694888e3c2057884ac195b0b8832d7b0
Rob Berry <rob@luvhate.us>
2009-08-28 00:06:06 +0100

    changed which post attributes are indexed

--- feeds/search_indexes.py
+++ feeds/search_indexes.py
@@ -6,4 +6,8 @@ class PostIndex(indexes.SearchIndex):
 
     text = indexes.CharField(document=True, use_template=True)
     rendered = indexes.CharField(use_template=True, indexed=False)
+    title = indexes.CharField(model_attr='title')
+    summary = indexes.CharField(model_attr='summary')
+    link = indexes.CharField(model_attr='link')
+
 site.register(Post, PostIndex)
