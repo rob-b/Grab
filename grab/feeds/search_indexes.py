2298877e0baf56759aeff0844ee77e3b67cad67e
Rob Berry <rob@luvhate.us>
2009-09-04 00:39:34 +0100

    tweaks to the Post search indexing template

--- grab/feeds/search_indexes.py
+++ grab/feeds/search_indexes.py
@@ -5,9 +5,11 @@ from feeds.models import Post
 class PostIndex(indexes.SearchIndex):
 
     text = indexes.CharField(document=True, use_template=True)
-    rendered = indexes.CharField(use_template=True, indexed=False)
     title = indexes.CharField(model_attr='title')
     summary = indexes.CharField(model_attr='summary')
     link = indexes.CharField(model_attr='link')
+    rendered = indexes.CharField(use_template=True,
+                                 template_name='search/indexes/feeds/post_rendered.html',
+                                 indexed=False)
 
 site.register(Post, PostIndex)
