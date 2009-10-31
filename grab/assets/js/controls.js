337910a450f82b1bd8323e792f735f4f46c1610d
Rob Berry <rob@luvhate.us>
2009-10-31 21:44:28 +0000

    separate the document.ready function into smaller pieces

--- grab/assets/js/controls.js
+++ grab/assets/js/controls.js
@@ -1,70 +1,34 @@
 $(document).ready(function(){
-    var delay;
-    $('#content > ul').click(function(e){
-        clearTimeout(delay);
-        var li, target = $(e.target);
-        if (target.is('a.next-item')) {
-            target.parents('li:first').next().click();
-            e.preventDefault();
-            return false;
-        }
-        else if (target.is('a.prev-item')) {
-            target.parents('li:first').prev().click();
-            e.preventDefault();
-            return false;
-        }
-        else if (target.is('#content > ul li')) {
-            li = target;
-        }
-        else if (target.parents('li').length) {
-            li = target.parents('li:first');
-        }
 
-        if (target.is('a')) {
-            e.preventDefault();
-            activate_item(li);
-            delay = setTimeout(function(){
-                // window.location = target.attr('href');
-                window.open(target.attr('href'));
-            }, 1500);
-            return false;
-        }
-        else{
-            activate_item(li);
-        }
-    });
-    $('form.mark-as-read').each(function(){
-        $(this).ajaxForm();
-    })
+    search_form.init();
+    item_action.init();
 
-    var input = $('#feed-list form input');
-    var default_value = input.attr('value');
-    input.focus(function(){
-        if ($(this).attr('value') == default_value){
-            $(this).attr({value: ''});
-        }
-    })
-    input.blur(function(){
-        if ($(this).attr('value') == ''){
-            $(this).attr({value: default_value});
-        }
-    })
-
+    $('form.mark-as-read').ajaxForm();
     $('#scrollable').scrollable({
         vertical: true,
         size: 4,
         speed: 700,
     }).mousewheel();
-
-    // $('a[rel=popup]').each(function(){
-    //     var that = $(this);
-    //     that.click(function(){
-    //         window.open(that.attr('href'));
-    //         return false;
-    //     });
-    // });
 });
 
+var search_form = {
+    init: function(){
+        var input = $('#feed-list form input');
+        var default_value = input.attr('value');
+        input.focus(function(){
+            if ($(this).attr('value') == default_value){
+                $(this).attr({value: ''});
+            }
+        })
+        input.blur(function(){
+            if ($(this).attr('value') == ''){
+                $(this).attr({value: default_value});
+            }
+        })
+    }
+}
+
+/* item helpers */
 var activate_item = function(obj) {
     obj.addClass('active-item read');
     obj.siblings().removeClass('active-item');
@@ -74,5 +38,54 @@ var activate_item = function(obj) {
 
 var scroll_to = function(elem){
     var target_offset = elem.offset().top;
-    $('html,body').animate({scrollTop: target_offset}, {duration: 1000, easing: 'swing'} );
+    $('html,body').animate({scrollTop: target_offset}, {duration:1500, easing: 'easeOutExpo'} );
+}
+
+var item_action = {
+    activate_item: function(obj) {
+        obj.addClass('active-item read');
+        obj.siblings().removeClass('active-item');
+        obj.find('form.mark-as-read').submit();
+        self.scroll_to(obj);
+    },
+    scroll_to: function(elem){
+        var target_offset = elem.offset().top;
+        $('html,body').animate({scrollTop: target_offset}, {duration:1500, easing: 'easeOutExpo'} );
+    },
+    init: function(){
+        var delay;
+        $('#content > ul').click(function(e){
+            clearTimeout(delay);
+            var li, target = $(e.target);
+            if (target.is('a.next-item')) {
+                target.parents('li:first').next().click();
+                e.preventDefault();
+                return false;
+            }
+            else if (target.is('a.prev-item')) {
+                target.parents('li:first').prev().click();
+                e.preventDefault();
+                return false;
+            }
+            else if (target.is('#content > ul li')) {
+                li = target;
+            }
+            else if (target.parents('li').length) {
+                li = target.parents('li:first');
+            }
+
+            if (target.is('a')) {
+                e.preventDefault();
+                self.activate_item(li);
+                delay = setTimeout(function(){
+                    // window.location = target.attr('href');
+                    window.open(target.attr('href'));
+                }, 1500);
+                return false;
+            }
+            else{
+                self.activate_item(li);
+            }
+        });
+    }
 }
