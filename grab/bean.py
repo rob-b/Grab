d6c6e9d835d596c05385856d77b925a369cc8960
Rob Berry <rob@luvhate.us>
2009-11-22 14:21:12 +0000

    use beanstalk to process bg jobs

---
+++ grab/bean.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import beanstalkc
+import daemon
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+from feeds.tools import populate_feed
+from feeds.models import Feed
+
+log = open('probe.log', 'w+')
+def out(msg):
+    log.write('%s\n' % msg)
+
+def probe():
+    beanstalk = beanstalkc.Connection()
+    # log.close()
+    while True:
+        out("Waiting for job...")
+        job = beanstalk.reserve()
+        out("Job found")
+        try:
+            feed = Feed.objects.get(slug=job.body)
+        except Feed.DoesNotExist:
+            out('cannot find feed: %s\n' % job.body)
+        else:
+            list(populate_feed(feed))
+            out('updated %s\n' % feed)
+        job.delete()
+        log.flush()
+
+context = daemon.DaemonContext(working_directory=settings.PROJECT_PATH,
+                              stderr=open('errors.log', 'w+'))
+# with context:
+probe()
